ae7a28be74a982cc3473af446c8471fc849213dc
laixiaolai' <laiyongzhao@gmail.com>
2014-07-25 11:52:16 +0800

    update main.js
    add del_ff function

--- js/main.js
+++ js/main.js
@@ -386,3 +386,15 @@ $(function(){
     }).trigger("resize");
 
 });
+
+
+
+//把元素类型为空格而且是文本都删除
+function del_ff(elem){
+    var elem_child = elem.childNodes;
+    for(var i=0; i<elem_child.length;i++){
+        if(elem_child[i].nodeName == "#text" && !/\s/.test(elem_child.nodeValue)){
+            elem.removeChild(elem_child[i])
+        }
+    }
+}
